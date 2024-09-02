9e467129130d7a7a926a6695260c89f7e9fcd4e6
Avinash Pandey <Avinash-Chandra.Pandey@amd.com>
2024-09-02 09:03:05 +0000

    [ZENTORCH CORE] Added few optimization in SDPA kernel
    -- Added Bf16 buffer to store attention output
    -- Used bf16 kernel for move and copy operation
    
    Signed-off-by: Avinash Pandey <Avinash-Chandra.Pandey@amd.com>
    Change-Id: Id9fada0e35cc65e2dad5d625a5c31097bdc02940

--- src/cpu/cpp/kernels/zen_MaskedMultiHeadAttention_512.cpp
+++ src/cpu/cpp/kernels/zen_MaskedMultiHeadAttention_512.cpp
@@ -19,6 +19,7 @@
 #include "zen_MaskedMultiHeadAttention.hpp"
 
 #define AVX512_BF16_COMPUTE_ENABLE 1
+#define AVX512_BF16_STORE_ENABLE 1
 
 namespace zentorch {
 
@@ -763,6 +764,384 @@ scale_dot_product_for_indirect_access_kv_cache(
                          beam_idx);
 }
 
+// BF16 path
+std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor>
+scale_dot_product_for_indirect_access_kv_cache_bf16(
+    at::Tensor query, at::Tensor key, at::Tensor value, at::Tensor &key_cache,
+    at::Tensor &value_cache, at::Tensor &beam_idx, const int64_t offset,
+    const double scale_factor, at::Tensor &attention_mask) {
+
+  RECORD_FUNCTION(
+      "zentorch::scale_dot_product_for_indirect_access_kv_cache_bf16",
+      c10::ArrayRef<c10::IValue>({}));
+  int beam_batch = beam_idx.size(1);
+  auto bs = query.size(0);
+  auto cur_len = query.size(1); // only process cur_len==1
+  auto head_num = query.size(2);
+  auto kv_head = key.size(2);
+  auto group_size = head_num / kv_head;
+  auto head_size = query.size(3);
+  auto seq_len = offset + cur_len;
+  auto kc_token_stride = beam_batch * kv_head * head_size;
+  auto attn_weights = at::empty({bs, head_num, cur_len, seq_len}, at::kFloat);
+  query = query.contiguous();
+  key = key.contiguous();
+  auto q_ptr = query.data_ptr<at::BFloat16>();
+  auto k_ptr = key.data_ptr<at::BFloat16>();
+  auto k_cache_ptr = key_cache.data_ptr<at::BFloat16>();
+  auto mask_ptr = attention_mask.data_ptr<at::BFloat16>();
+  auto mask_head_num = attention_mask.size(1);
+  auto mask_dim2 = attention_mask.size(2);
+  auto mask_bs_stride = mask_head_num * mask_dim2 * seq_len;
+  // value realted
+  value = value.contiguous();
+  auto attn_outs =
+      at::empty({bs, head_num, cur_len, head_size}, value.options());
+  auto v_ptr = value.data_ptr<at::BFloat16>();
+  auto v_cache_ptr = value_cache.data_ptr<at::BFloat16>();
+  auto attn_out_ptr = attn_outs.data_ptr<at::BFloat16>();
+  // zentorch::zero_ker(attn_out_ptr, attn_outs.numel());
+  auto attn_w_ptr = attn_weights.data_ptr<float>();
+  auto thread_numbers = omp_get_max_threads();
+  auto max_parallel_parts = thread_numbers * 4;
+
+  // TODO: Generate more heuristics based on bs, seq_len
+  // and device cache capacity. Decide more fine grain
+  // kv_block_size for lower bs. Current target_block_size
+  // works optimally for bs >=4.
+  auto target_block_size = 128L;
+  if (bs <= 8 and seq_len < 65536) {
+    target_block_size = 32L;
+  }
+  auto kv_block_size = bs * head_num >= max_parallel_parts
+                           ? seq_len
+                           : std::max(seq_len / max_parallel_parts, 1L);
+  kv_block_size = std::min(kv_block_size, target_block_size);
+  auto kv_block_count = (seq_len + kv_block_size - 1) / kv_block_size;
+  auto need_update_beam_idx = offset > 0 && bs > 1;
+  auto b_ptr = beam_idx.data_ptr<long>();
+  auto max_cache_size = beam_idx.size(0);
+  long new_beam_idx[beam_batch][offset + query.size(1) + 1] = {};
+  auto prompt_len = b_ptr[(max_cache_size - 2) * beam_batch];
+  auto prompt_bs = b_ptr[(max_cache_size - 1) * beam_batch];
+  auto beam_size = beam_batch / prompt_bs;
+
+  if (need_update_beam_idx) {
+    // according to the last decoded token to get the target beam for the past
+    // token
+#pragma omp parallel for
+    for (int i = 0; i < bs; i++) {
+      new_beam_idx[i][offset - 1] = b_ptr[(offset - 1) * bs + i];
+      for (int j = offset - 2; j >= 0;
+           j--) { // for the token of input, the target beam is alwarys 0
+        if (j < prompt_len - 1 && bs == beam_size)
+          break; // fast path for latency mode
+        new_beam_idx[i][j] = b_ptr[j * bs + new_beam_idx[i][j + 1]];
+      }
+    }
+  }
+  {
+    RECORD_FUNCTION("zentorch::iakv_sdp::matmul(query, key)",
+                    c10::ArrayRef<c10::IValue>({}));
+#pragma omp parallel for collapse(3)
+    for (auto block_id = 0; block_id < kv_block_count; block_id++) {
+      for (auto bi = 0; bi < bs; bi++) {
+        for (auto hi = 0; hi < head_num; hi++) {
+          auto k_start = block_id * kv_block_size;
+          auto block_size = std::min(kv_block_size, seq_len - k_start);
+          auto query_ti = 0;
+          for (auto ti = k_start; ti < k_start + block_size; ti++) {
+            auto kv_hi = hi / group_size; // maping the query head to
+                                          // key/value head to support MGA/MQA
+            auto q_ptr_start =
+                q_ptr + (bi * cur_len + query_ti) * head_num * head_size +
+                hi * head_size;
+            auto attn_w_stride = (bi * head_num + hi) * cur_len * seq_len;
+            auto attn_w_pos =
+                attn_w_ptr + attn_w_stride + query_ti * seq_len + ti;
+            attn_w_pos[0] = 0.0f;
+            auto kc_token_start = ti * kc_token_stride;
+            auto kc_t_beam_start = kc_token_start;
+            auto beam = need_update_beam_idx ? new_beam_idx[bi][ti] : 0;
+            if (ti > query_ti + offset) { // only caculate the innerproduct for
+                                          // the past token and current token
+              attn_w_pos[0] = -10000.0f;
+            } else if (ti == query_ti + offset) { // caculate the innerproduct
+                                                  // for the current token and
+                                                  // store the key
+              if (cur_len > 1) { // this may occur for processing the promt
+                auto beam_size = beam_batch / bs;
+                // need to store key accross beam
+                kc_t_beam_start =
+                    kc_t_beam_start + bi * beam_size * kv_head * head_size;
+              } else {
+                kc_t_beam_start = kc_t_beam_start + bi * kv_head * head_size;
+              }
+              auto kc_head_start =
+                  k_cache_ptr + kc_t_beam_start + kv_hi * head_size;
+              auto k_ptr_start =
+                  k_ptr + (bi * cur_len + ti - offset) * kv_head * head_size +
+                  kv_hi * head_size;
+              reduce_head<at::BFloat16>(q_ptr_start, k_ptr_start, attn_w_pos,
+                                        head_size, true, kc_head_start);
+            } else { // caculate the innerproduct for the past token
+              if (ti >= offset) {
+                auto k_ptr_start =
+                    k_ptr + (bi * cur_len + ti - offset) * kv_head * head_size +
+                    kv_hi * head_size;
+                reduce_head<at::BFloat16>(q_ptr_start, k_ptr_start, attn_w_pos,
+                                          head_size, false, nullptr);
+              } else {
+                kc_t_beam_start = kc_t_beam_start + beam * kv_head * head_size;
+                if (cur_len > 1) {
+                  auto beam_size = beam_batch / bs;
+                  kc_t_beam_start =
+                      kc_t_beam_start + bi * beam_size * kv_head * head_size;
+                }
+                auto kc_head_start =
+                    k_cache_ptr + kc_t_beam_start + kv_hi * head_size;
+                reduce_head<at::BFloat16>(q_ptr_start, kc_head_start,
+                                          attn_w_pos, head_size, false,
+                                          nullptr);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  {
+    RECORD_FUNCTION("zentorch::iakv_sdp::div_add_softmax",
+                    c10::ArrayRef<c10::IValue>({}));
+#pragma omp parallel for collapse(2)
+    for (auto bi = 0; bi < bs; bi++) {
+      for (auto hi = 0; hi < head_num; hi++) {
+        for (auto query_ti = 0; query_ti < cur_len; query_ti++) {
+          auto mask_ptr_start = mask_ptr + bi * mask_bs_stride +
+                                (hi % mask_head_num) * mask_dim2 * seq_len;
+          auto attn_w_stride = (bi * head_num + hi) * cur_len * seq_len;
+          auto attn_w_query_start =
+              attn_w_ptr + attn_w_stride + query_ti * seq_len;
+// div+add+softmax
+#if defined(CPU_CAPABILITY_AVX512)
+          for (auto qi = 0; qi < 1; qi++) {
+            auto max_val = -100000.0f;
+            zentorch::_dil_div_add_reduce_max_fusion_kernel<float,
+                                                            at::BFloat16>(
+                attn_w_query_start,
+                mask_ptr_start + (query_ti % mask_dim2) * seq_len, scale_factor,
+                seq_len, attn_w_query_start, max_val);
+
+            zentorch::_dil_exp_reduce_sum_fusion_kernel(
+                attn_w_query_start, seq_len, attn_w_query_start, max_val);
+            zentorch::_dil_normalization_kernel<float>(
+                attn_w_query_start, max_val, seq_len, attn_w_query_start);
+          }
+#else
+          for (auto qi = 0; qi < 1; qi++) {
+            auto max_val = -100000.0f;
+            // div+add and find max
+            for (auto si = 0; si < seq_len; si++) {
+              attn_w_query_start[si] =
+                  attn_w_query_start[si] / scale_factor +
+                  mask_ptr_start[(query_ti % mask_dim2) * seq_len + si];
+              if (attn_w_query_start[si] > max_val) {
+                max_val = attn_w_query_start[si];
+              }
+            }
+            // softmax
+            float sum = 0.0f;
+            // exp and sum
+            for (auto si = 0; si < seq_len; si++) {
+              attn_w_query_start[si] = exp(attn_w_query_start[si] - max_val);
+              sum += attn_w_query_start[si];
+            }
+            // normalization
+            for (auto si = 0; si < seq_len; si++) {
+              attn_w_query_start[si] = attn_w_query_start[si] / sum;
+            }
+          }
+#endif
+        }
+      }
+    }
+  }
+#if AVX512_BF16_STORE_ENABLE
+  auto private_attn_outs =
+      at::empty({thread_numbers, bs, head_num, cur_len, head_size}, at::kHalf);
+#else
+  auto private_attn_outs =
+      at::empty({thread_numbers, bs, head_num, cur_len, head_size}, at::kFloat);
+#endif
+  auto private_attn_out_flag =
+      at::zeros({thread_numbers, bs, head_num}, at::kByte);
+  auto flag_access = private_attn_out_flag.accessor<uint8_t, 3>();
+#if AVX512_BF16_STORE_ENABLE
+  auto private_attn_out_ptr =
+      (at::BFloat16 *)private_attn_outs.data_ptr<at::Half>();
+#else
+  auto private_attn_out_ptr = private_attn_outs.data_ptr<float>();
+#endif
+  // private_attn_outs.numel());
+  auto attn_outs_stride_priv = bs * head_num * cur_len * head_size;
+  {
+    RECORD_FUNCTION("zentorch::iakv_sdp::matmul(attn_w, value)",
+                    c10::ArrayRef<c10::IValue>({}));
+// TODO Enable BF16 compute for mul_attenion_weights_and_value_of_head
+#pragma omp parallel for collapse(3)
+    for (auto block_id = 0; block_id < kv_block_count; block_id++) {
+      for (auto bi = 0; bi < bs; bi++) {
+        for (auto hi = 0; hi < head_num; hi++) {
+          auto thread_id = 0;
+          if (kv_block_size < seq_len)
+            thread_id = omp_get_thread_num();
+          auto v_start = block_id * kv_block_size;
+          auto block_size = std::min(kv_block_size, seq_len - v_start);
+          auto query_ti = 0;
+          for (auto vi = v_start; vi < v_start + block_size; vi++) {
+            auto kv_hi = hi / group_size; // maping the query head to
+                                          // key/value head to support MGA/MQA
+            auto attn_w_stride = (bi * head_num + hi) * cur_len * seq_len;
+            auto attn_w_query_start =
+                attn_w_ptr + attn_w_stride + query_ti * seq_len;
+            // calculate weighted value and store the result to attn_outs[bs,
+            // head_num, cur_len, head_size]
+            auto attn_out_head_stride =
+                thread_id * attn_outs_stride_priv +
+                (bi * head_num + hi) * cur_len * head_size;
+            auto attn_out_start = private_attn_out_ptr + attn_out_head_stride +
+                                  query_ti * head_size;
+
+            auto vc_token_start = vi * kc_token_stride;
+            auto beam = need_update_beam_idx ? new_beam_idx[bi][vi] : 0;
+            if (vi == query_ti + offset) { // caculate the attention values
+                                           // for the current token
+              auto vc_t_beam_start = vc_token_start;
+              if (cur_len > 1) { // this may occur for processing the promt
+                auto beam_size = beam_batch / bs;
+                // removed the redundant computation, need to store key
+                // accross beam
+                vc_t_beam_start =
+                    vc_t_beam_start + bi * beam_size * kv_head * head_size;
+              } else {
+                vc_t_beam_start = vc_t_beam_start + bi * kv_head * head_size;
+              }
+              auto v_cache_head_start =
+                  v_cache_ptr + vc_t_beam_start + kv_hi * head_size;
+              auto v_ptr_start =
+                  v_ptr + (bi * cur_len + vi - offset) * kv_head * head_size +
+                  kv_hi * head_size;
+#if AVX512_BF16_STORE_ENABLE
+              mul_attenion_weights_and_value_of_head<at::BFloat16,
+                                                     at::BFloat16>(
+                  attn_w_query_start[vi], v_ptr_start, attn_out_start,
+                  head_size, true, v_cache_head_start,
+                  flag_access[thread_id][bi][hi]);
+#else
+              mul_attenion_weights_and_value_of_head<at::BFloat16, float>(
+                  attn_w_query_start[vi], v_ptr_start, attn_out_start,
+                  head_size, true, v_cache_head_start,
+                  flag_access[thread_id][bi][hi]);
+#endif
+            } else if (vi < query_ti + offset) { // caculate attention
+                                                 // values for the past
+                                                 // token
+              if (vi >= offset) {
+                auto v_ptr_start =
+                    v_ptr + (bi * cur_len + vi - offset) * kv_head * head_size +
+                    kv_hi * head_size;
+#if AVX512_BF16_STORE_ENABLE
+                mul_attenion_weights_and_value_of_head<at::BFloat16,
+                                                       at::BFloat16>(
+                    attn_w_query_start[vi], v_ptr_start, attn_out_start,
+                    head_size, false, nullptr, flag_access[thread_id][bi][hi]);
+#else
+                mul_attenion_weights_and_value_of_head<at::BFloat16, float>(
+                    attn_w_query_start[vi], v_ptr_start, attn_out_start,
+                    head_size, false, nullptr, flag_access[thread_id][bi][hi]);
+#endif
+              } else {
+                auto vc_t_beam_start =
+                    vc_token_start + beam * kv_head * head_size;
+                if (cur_len > 1) {
+                  auto beam_size = beam_batch / bs;
+                  vc_t_beam_start =
+                      vc_t_beam_start + bi * beam_size * kv_head * head_size;
+                }
+                auto v_cache_head_start =
+                    v_cache_ptr + vc_t_beam_start + kv_hi * head_size;
+#if AVX512_BF16_STORE_ENABLE
+                mul_attenion_weights_and_value_of_head<at::BFloat16,
+                                                       at::BFloat16>(
+                    attn_w_query_start[vi], v_cache_head_start, attn_out_start,
+                    head_size, false, nullptr, flag_access[thread_id][bi][hi]);
+#else
+                mul_attenion_weights_and_value_of_head<at::BFloat16, float>(
+                    attn_w_query_start[vi], v_cache_head_start, attn_out_start,
+                    head_size, false, nullptr, flag_access[thread_id][bi][hi]);
+#endif
+              }
+            }
+            if (flag_access[thread_id][bi][hi] == 0)
+              flag_access[thread_id][bi][hi] = 1;
+          }
+        }
+      }
+    }
+  }
+
+  {
+    RECORD_FUNCTION("zentorch::iakv_sdp::reduction_private_result",
+                    c10::ArrayRef<c10::IValue>({}));
+    // TODO Enable AVX512  BF16 load, store and add for add and move kernel
+#pragma omp parallel for collapse(3)
+    for (auto bi = 0; bi < bs; bi++) {
+      for (auto hi = 0; hi < head_num; hi++) {
+        for (auto qi = 0; qi < cur_len; qi++) {
+          auto thr0_head_start = private_attn_out_ptr +
+                                 (bi * head_num + hi) * cur_len * head_size +
+                                 qi * head_size;
+          if (flag_access[0][bi][hi] == 0) {
+            zentorch::zero_ker(thr0_head_start, head_size);
+          }
+          if (kv_block_size < seq_len) {
+            for (auto thread_id = 1; thread_id < thread_numbers; thread_id++) {
+              if (flag_access[thread_id][bi][hi] == 0) {
+                continue;
+              }
+              auto attn_out_head_stride =
+                  thread_id * attn_outs_stride_priv +
+                  (bi * head_num + hi) * cur_len * head_size;
+              auto private_attn_out_start =
+                  private_attn_out_ptr + attn_out_head_stride + qi * head_size;
+#if AVX512_BF16_STORE_ENABLE
+              zentorch::add_ker<at::BFloat16, at::BFloat16>(
+                  thr0_head_start, private_attn_out_start, head_size);
+#else
+              zentorch::add_ker<float, float>(
+                  thr0_head_start, private_attn_out_start, head_size);
+#endif
+            }
+          }
+          auto attn_outs_start = attn_out_ptr +
+                                 (bi * head_num + hi) * cur_len * head_size +
+                                 qi * head_size;
+#if AVX512_BF16_STORE_ENABLE
+          zentorch::move_ker<at::BFloat16, at::BFloat16>(
+              attn_outs_start, thr0_head_start, head_size);
+#else
+          zentorch::move_ker<at::BFloat16, float>(attn_outs_start,
+                                                  thr0_head_start, head_size);
+#endif
+        }
+      }
+    }
+  }
+
+  return std::make_tuple(attn_outs, at::Tensor(), key_cache, value_cache,
+                         beam_idx);
+}
+
 std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor>
 zero_copy_kv_cache_masked_multihead_self_attention_kernel_impl(
     at::Tensor query, at::Tensor key, at::Tensor value, at::Tensor &key_cache,
@@ -787,8 +1166,7 @@ zero_copy_kv_cache_masked_multihead_self_attention_kernel_impl(
         query, key, value, key_cache, value_cache, beam_idx, offset, scale_attn,
         attention_mask);
   }
-  return scale_dot_product_for_indirect_access_kv_cache<at::BFloat16,
-                                                        at::BFloat16>(
+  return scale_dot_product_for_indirect_access_kv_cache_bf16(
       query, key, value, key_cache, value_cache, beam_idx, offset, scale_attn,
       attention_mask);
 }
