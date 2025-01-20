07f24ba77a2f839b76f326d320bb4b3111e3095d
durga <venkatadurgarao.gambali@amd.com>
2025-01-20 05:53:17 -0700

    [ZENTORCH CORE] Fixes ZENAI-683
    -- Fixes unitest failures with Pytorch 2.1.2
    -- Update unittest steps in Readme.md
    -- JIRA ID: ZENAI-683
    
    Signed-off-by: durga <venkatadurgarao.gambali@amd.com>
    Change-Id: I8b95a07f538da42d481b50e1ae4f99c41cfd9655
    Signed-off-by: durga <venkatadurgarao.gambali@amd.com>
    (cherry picked from commit dd86f1e16d5ccd66c076c3c70ef6f8943a4f116a)

--- test/install_requirements.py
+++ test/install_requirements.py
@@ -1,29 +1,53 @@
 # ******************************************************************************
-# Copyright (c) 2023-2024 Advanced Micro Devices, Inc.
+# Copyright (c) 2023-2025 Advanced Micro Devices, Inc.
 # All rights reserved.
 # ******************************************************************************
 
 import importlib.util as importutil
-
-import pip
 import torch
+import subprocess
 
 _all_ = ["transformers", "expecttest==0.1.6", "parameterized"]
 
 
+def install_package(cmd):
+    """
+    Installs a package using the provided command.
+
+    This function uses subprocess.Popen to run the given command in a new
+    subprocess, which helps to avoid conflicts that might arise from running
+    pip.main() within the current Python process.
+    Args:
+        cmd (str): The command to run for installing the package, typically a
+        pip install command.
+    Returns:
+        tuple: A tuple containing the return code (int), standard output (str),
+        and standard error (str).
+    """
+    p1 = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p1.communicate()
+    rc = p1.returncode
+    return rc, out.decode("ascii", "ignore"), err.decode("ascii", "ignore")
+
+
 def install(packages):
     for package in packages:
-        pip.main(["install", package])
+        rc, out, err = install_package("pip install %s" % package)
+        if rc != 0:
+            print("Issue while installing the package=%s" % package)
+            print(err)
+            exit(1)
+        else:
+            print(out)
 
 
 if __name__ == "__main__":
 
     install(_all_)
-
     extra_args = (
-        ["--index-url", "https://download.pytorch.org/whl/cpu"]
+        " --index-url https://download.pytorch.org/whl/cpu"
         if not torch.version.cuda
-        else []
+        else ""
     )
     torch_version = torch.__version__
     torch_version = torch_version.split("+")[0]
@@ -46,7 +70,8 @@ if __name__ == "__main__":
         print("Warning: Torchvision already installed, skipping installing it")
         exit(1)
     elif torch_version in torchvision_compatibilty.keys():
-        pip.main(["install", torchvision_compatibilty[torch_version], *extra_args])
+        torchvision_cmd = [torchvision_compatibilty[torch_version] + extra_args]
+        install(torchvision_cmd)
     else:
         print(
             "Couldnot find the valid torchvision version which is \
