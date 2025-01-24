2ddb9b3beb409c367a9541fd8800617539e9f22a
durga <venkatadurgarao.gambali@amd.com>
2025-01-24 10:45:22 +0000

    [ZENTORCH CORE] Add deprecated pkg to setup.py
    -- Fixes ZENAI-732
    -- Add deprecated pkg to wheel file dependency in setup.py
    
    Signed-off-by: durga <venkatadurgarao.gambali@amd.com>
    Change-Id: Id3e1160bda16c5709cbe0b1d27088e8ce305632e

--- setup.py
+++ setup.py
@@ -165,7 +165,7 @@ include_dirs = [
 
 zentorch_build_version = os.getenv("ZenTorch_BUILD_VERSION", PACKAGE_VERSION)
 git_sha = get_commit_hash(project_root_dir)
-wheel_file_dependencies = ["numpy", "torch"]
+wheel_file_dependencies = ["numpy", "torch", "deprecated"]
 # -Wno-unknown-pragma is for [unroll pragma], to be removed
 # -fopenmp is needed for omp related pragmas (simd etc.)
 extra_compile_args = [
