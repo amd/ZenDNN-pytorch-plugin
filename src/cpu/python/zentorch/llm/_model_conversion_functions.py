1b7c2402483f15fa55077dcb0f7629dcc2b250c5
Chinmay Kulkarni <Chinmay.Kulkarni@amd.com>
2025-03-26 04:42:11 -0400

    Revert "[ZENTORCH CORE] Implement Zentorch_moe op"
    This reverts commit c06f6407ef05d0876a525611ba0c8ce8f62d5962.
    
    Reason for revert:
     -- Analysis for this model was done with an older version of the
        transformers package.
     -- With the newer version of the transformers package a slight drop in
        accuracy is observed. Hence reverting the patch.
     -- ZENAI-0000
    
    Change-Id: Ibf4893b9b550ba9210de3481e89b550fd05e9886
    Signed-off-by: Vasanta YaminiBitra <vasanta.yaminibitra@amd.com>
    Signed-off-by: Chinmay Kulkarni <chkulkar@amd.com>

--- src/cpu/python/zentorch/llm/_model_conversion_functions.py
+++ src/cpu/python/zentorch/llm/_model_conversion_functions.py
@@ -231,20 +231,6 @@ def customize_model(model):
         models.MistralModel_forward = (
             MistralModel_forward
         )
-
-    if model.config.architectures[0] == "MixtralForCausalLM":
-        from intel_extension_for_pytorch.transformers.models.reference.modules import (
-            decoder,
-        )
-        from intel_extension_for_pytorch.transformers.models.reference import (
-            models,
-        )
-        from ._custom_model_forward import (
-            MixtralDecoderLayer_forward, MixtralModel_forward
-        )
-        decoder.MixtralDecoderLayer_forward, models.MixtralModel_forward = (
-            MixtralDecoderLayer_forward, MixtralModel_forward
-        )
     # Over riding forward of RotaryEmbedding class for modifying
     # the longrope op to address graph breaks for v2.4 and above.
     installed_ipex_version = get_installed_ipex_version()
