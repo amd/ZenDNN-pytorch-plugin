0e99ba7120870bca2cf1759f4de3cca0294ae0fa
Mrigank Srivastava <Mrigank.Srivastava@amd.com>
2025-07-04 05:42:49 -0600

    [ZENTORCH CORE] Fix freezing for OOM errors.
    - added conditional restoration.
    - JIRA ID: ZENAI-1700.
    
    Signed-off-by: Mrigank Srivastava <Mrigank.Srivastava@amd.com>
    Change-Id: Ie4ffc2dffd9a7cc784b7998d62bd9927a150bf59
    (cherry picked from commit 0d0611a61862bc42d6abc40e2178ecf3edb808c3)

--- src/cpu/python/zentorch/_freeze_utils.py
+++ src/cpu/python/zentorch/_freeze_utils.py
@@ -14,17 +14,22 @@ def freezing_enabled():
     # read previous/default values of freeze, this works even if
     # the user has set the values manually as we restore to that value
     previous_freeze_config = torch._inductor.config.freezing
-    previous_freeze_path = torch._inductor.freezing.freeze
     # monkey patch pytorch freeze
     torch._inductor.config.freezing = True
-    if is_version_compatible_import(["_inductor", "freezing"], ["freeze"]):
-        # PT 2.6
-        # TODO: remove this if block when dropping support for PT 2.6
-        torch._inductor.freezing.freeze = _freeze
-    else:
+    if is_version_compatible_import(["_inductor", "freezing"], ["_freeze"]):
         # PT 2.7 or above
+        previous_freeze_path = torch._inductor.freezing._freeze
         torch._inductor.freezing._freeze = _freeze
+    else:
+        # PT 2.6
+        # TODO: remove this else block when dropping support for PT 2.6
+        previous_freeze_path = torch._inductor.freezing.freeze
+        torch._inductor.freezing.freeze = _freeze
     yield
+    if is_version_compatible_import(["_inductor", "freezing"], ["_freeze"]):
+        torch._inductor.freezing._freeze = previous_freeze_path
+    else:
+        # TODO: remove this else block when dropping support for PT 2.6
+        torch._inductor.freezing.freeze = previous_freeze_path
     # reset to the previous values
     torch._inductor.config.freezing = previous_freeze_config
-    torch._inductor.freezing.freeze = previous_freeze_path
