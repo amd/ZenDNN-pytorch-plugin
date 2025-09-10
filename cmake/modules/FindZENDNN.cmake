969c3c0720772e20dbecd38a0447fd1a68bd0556
Ravipati Chandramalika <Ravipati.Chandramalika@amd.com>
2025-09-09 22:48:09 -0600

    [ZENTORCH INFRA] Update zendnn tag for zentorch
    -- Update zendnn tag in zentorch
    -- ZENAI-1891
    
    Change-Id: I6a38b7a5e3c8080c2720c49fe4b7fef054b2eaa8
    Signed-off-by: Ravipati Chandramalika <Ravipati.Chandramalika@amd.com>

--- cmake/modules/FindZENDNN.cmake
+++ cmake/modules/FindZENDNN.cmake
@@ -187,7 +187,7 @@ IF("$ENV{ZENTORCH_USE_LOCAL_ZENDNN}" EQUAL 0)
 
     FetchContent_Declare(ZenDNN
     GIT_REPOSITORY https://github.com/amd/ZenDNN.git
-    GIT_TAG zendnn-2025-WW35
+    GIT_TAG zendnn-2025-WW36
     SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third_party/ZenDNN"
     SOURCE_SUBDIR "not-available"
     )
