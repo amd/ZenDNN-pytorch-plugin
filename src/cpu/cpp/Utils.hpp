5bdc207345bdc478852db7d3ab985f7ac8132cb1
Adhav, Harshal <Harshal.Adhav@amd.com>
2026-08-27 16:36:37 +0530

    [ZENTORCH INFRA] Apply clang-format to ZENTORCH_CHECK macro in Utils.hpp (#625)
    Reformat the ZENTORCH_CHECK macro continuation lines to satisfy
    git clang-format with clang-format 23.x (ColumnLimit: 80). No
    functional change.
    
    Change-Id: I1f1a5c4c60a38c8e0f4b265b9d41705f08053f0b
    
    Signed-off-by: Harshal Adhav <harshal.adhav@amd.com>

--- src/cpu/cpp/Utils.hpp
+++ src/cpu/cpp/Utils.hpp
@@ -19,8 +19,8 @@
 // TODO: Make the __FILE__ give the name of the file relative to only
 // ZenDNN_PyTorch_Plugin
 #define ZENTORCH_CHECK(condition, ...)                                         \
-  TORCH_CHECK(condition, __FILE__, ":", __LINE__, " ", __FUNCTION__, " : ",    \
-              ##__VA_ARGS__)
+  TORCH_CHECK(condition, __FILE__, ":", __LINE__, " ", __FUNCTION__,           \
+              " : ", ##__VA_ARGS__)
 
 namespace zentorch {
 
